0a4e6110033476b1709e58b4b3c226a47e1290f1
akaneNK <rabiooron19@gmail.com>
2024-12-14 15:19:21 +0900

    削除

--- backend/app/config.py
+++
@@ -1,5 +0,0 @@
-# 設定ファイル
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost:3306/mycloset01'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    OPENAI_API_KEY = 'your_openai_api_key'
