cc8a141777f12351e9000d4f8665e3c9b56a3079
matsumo <musmarpo@gmail.com>
2025-01-03 15:53:24 +0900

    レイアウトの修正（？）
    このレイアウト部分を修正した記憶はないのですが、変な部分があったら教えてください

--- frontend/src/app/components/Outfits/Outfits1/MessageBox.tsx
+++ frontend/src/app/components/Outfits/Outfits1/MessageBox.tsx
@@ -2,18 +2,19 @@
 
 import { Button, Box } from "@chakra-ui/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-const MessageBox = () => {
+type Props = {
+  clickEvent?: () => void;
+};
+
+const MessageBox: React.FC<Props> = ({ clickEvent, ...props }) => {
   const router = useRouter();
-
-  const handleClick = () => {
-    router.push("/outfits2"); // outfits2ページに遷移
-  };
 
   return (
     <Box p={4} textAlign="center">
       <Button
-        onClick={handleClick}
+        onClick={clickEvent}
         bg="green.400" // 背景色
         color="gray.700" // テキスト色
         _hover={{ bg: "orange.500" }} // ホバー時の背景色
@@ -22,7 +23,7 @@ const MessageBox = () => {
         rounded="lg" // 角丸
         transition="0.2s" // トランジション時間
       >
-        何着て行こうかな？
+        何着ようかな？
       </Button>
     </Box>
   );
